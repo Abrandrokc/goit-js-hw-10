c20139e2756e94b88d91ce42900e32658238b9de
Abrandrokc <Abrandrokco@gmail.com>
2024-03-02 10:42:30 +0200

    12

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,44 +1,41 @@
 import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css"; 
+import "izitoast/dist/css/iziToast.min.css";
+
 function createPromise(delay, state) {
-      return new Promise((resolve, reject) => {
-        if (state === 'fulfilled') {
-          setTimeout(() => {
-            resolve(delay);
-          }, delay);
-        } else if (state === 'rejected') {
-          setTimeout(() => {
-            reject(delay);
-          }, delay);
-        }
-      });
+  return new Promise((resolve, reject) => {
+    if (state === 'fulfilled') {
+      setTimeout(() => {
+        resolve(delay);
+      }, delay);
+    } else if (state === 'rejected') {
+      setTimeout(() => {
+        reject(delay);
+      }, delay);
     }
-const sumbit = document.querySelector('button[type="submit"]');
-const input = document.querySelector('[name="delay"]');
+  });
+}
 
+const form = document.querySelector('.form');
+const delayInput = document.querySelector('[name="delay"]');
 
-   sumbit.addEventListener('click', function(event) {
-     event.preventDefault(); 
-      const delay = parseInt(input.value);
-     const state = document.querySelector('input[name="state"]:checked').value;
-     
-     
-      createPromise(delay, state)
-        .then((delay) => {
-          
-          iziToast.success({
-            title: 'Success',
-            message: `✅ Fulfilled promise in ${delay}ms`,
-                position: 'topRight',
-          });
-        })
-        .catch((delay) => {
-        
-          iziToast.error({
-            title: 'Error',
-            message: `❌ Rejected promise in ${delay}ms`,
-            position: 'topRight',
-          });
-        });
-   });
-    
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+  const delay = parseInt(delayInput.value);
+  const state = document.querySelector('input[name="state"]:checked').value;
+  
+  createPromise(delay, state)
+    .then((delay) => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch((delay) => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
